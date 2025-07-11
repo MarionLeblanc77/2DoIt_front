4bc44479f24716210f19f07b611a38669b5f969f
Marion Leblanc <marion.leblanc77@gmail.com>
2025-07-11 16:03:43 +0200

    Readme

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,8 +4,11 @@ module.exports = {
     "airbnb",
     "airbnb-typescript",
     "airbnb/hooks",
+    "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
     "plugin:prettier/recommended", // must be the last one
   ],
   parser: "@typescript-eslint/parser",
@@ -15,7 +18,7 @@ module.exports = {
     },
     ecmaVersion: "latest",
     sourceType: "module",
-    project: "./tsconfig.json",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
   },
   overrides: [
     {
