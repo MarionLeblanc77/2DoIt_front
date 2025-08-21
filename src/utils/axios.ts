edba81db0d117b2156ee4a6c54a02c5a34e09ab1
Marion Leblanc <marion.leblanc77@gmail.com>
2025-08-21 19:18:04 +0200

    Ok cors

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -8,16 +8,16 @@ const getBackendEndpoint = (): string => {
   return "http://127.0.0.1:8000";
 };
 
-const getEnv = (): string => {
-  if (typeof window !== "undefined" && window.APP_CONFIG) {
-    return window.APP_CONFIG.APP_ENV || "dev";
-  }
-  return "dev";
-};
+// const getEnv = (): string => {
+//   if (typeof window !== "undefined" && window.APP_CONFIG) {
+//     return window.APP_CONFIG.APP_ENV || "dev";
+//   }
+//   return "dev";
+// };
 
 export const axiosInstance = axios.create({
   baseURL: `${getBackendEndpoint()}/api`,
-  withCredentials: getEnv() === "prod",
+  // withCredentials: getEnv() === "prod",
 });
 
 export const addTokenToAxiosInstance = (token: string) => {
